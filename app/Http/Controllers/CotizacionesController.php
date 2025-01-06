7df60a464967a9bf415743713ab5b90b18a1da64
Carlos Mac <carlos.alejandro89@outlook.com>
2025-01-06 11:39:10 -0600

    Cambio de estatus al asignar empresa

--- app/Http/Controllers/CotizacionesController.php
+++ app/Http/Controllers/CotizacionesController.php
@@ -866,7 +866,7 @@ class CotizacionesController extends Controller
                 ->where('id', $c->IdContenedor)
                 ->update([
                     'id_empresa' => $request->empresa,
-                    'estatus' => 'Aprobada'
+                    'estatus' => 'En Espera'
                 ]);
         
                 $contenedores = DB::table('docum_cotizacion')
