d83b54c6a95e211f786ce2d3740ce250ef02dde2
carlosalejandro <carlos.alejandro89@outlook.com>
2025-09-07 22:16:26 -0600

    Finalizando

--- resources/views/cotizaciones/externos/datos_facturacion.blade.php
+++ resources/views/cotizaciones/externos/datos_facturacion.blade.php
@@ -13,7 +13,7 @@
           <div class="text-muted fs-7">Agregue la información para elaborar Carta Porte</div>
           <div class="mb-2"></div>
           <div class="fv-row row mb-3">
-            <div class="col-6">
+            <div class="col-6 d-none">
               <div class="input-group  mb-5">
                   <span class="input-group-text" id="basic-addon1">
                       <i class="ki-duotone ki-route fs-1">
@@ -24,13 +24,13 @@
                       </i>
                   </span>
                   <div class="form-floating">
-                      <input @if ($action == 'editar') value="" @endif type="text"
+                      <input @if ($action == 'editar') value="." @endif type="text"
                           class="form-control" id="destino" autocomplete="off" placeholder="" />
                       <label for="destino" class="text-gray-700">Razón Social</label>
                   </div>
               </div>
             </div>
-            <div class="col-3">
+            <div class="col-3 d-none">
               <div class="input-group  mb-5">
                   <span class="input-group-text" id="basic-addon1">
                       <i class="ki-duotone ki-route fs-1">
@@ -41,7 +41,7 @@
                       </i>
                   </span>
                   <div class="form-floating">
-                      <input @if ($action == 'editar') value="" @endif type="text"
+                      <input @if ($action == 'editar') value="." @endif type="text"
                           class="form-control" id="destino" autocomplete="off" placeholder="" />
                       <label for="destino" class="text-gray-700">RFC</label>
                   </div>
@@ -50,7 +50,7 @@
             
           </div>
           <div class="fv-row row mb-3">
-          <div class="col-12">
+          <div class="col-12 d-none">
               <div class="input-group  mb-5">
                   <span class="input-group-text" id="basic-addon1">
                       <i class="ki-duotone ki-route fs-1">
@@ -61,7 +61,7 @@
                       </i>
                   </span>
                   <div class="form-floating">
-                      <input @if ($action == 'editar') value="" @endif type="text"
+                      <input @if ($action == 'editar') value="." @endif type="text"
                           class="form-control" id="destino" autocomplete="off" placeholder="" />
                       <label for="destino" class="text-gray-700">Domicilio Fiscal</label>
                   </div>
@@ -80,8 +80,8 @@
                   </span>
                   <div class="form-floating">
                       <input @if ($action == 'editar') value="" @endif type="text"
-                          class="form-control" id="destino" autocomplete="off" placeholder="" />
-                      <label for="destino" class="text-gray-700">Pedimento</label>
+                          class="form-control" id="cp_fraccion" autocomplete="off" placeholder="" />
+                      <label for="cp_fraccion" class="text-gray-700">Fracción</label>
                   </div>
               </div>
             </div>
@@ -98,8 +98,8 @@
                   </span>
                   <div class="form-floating">
                       <input @if ($action == 'editar') value="" @endif type="text"
-                          class="form-control" id="destino" autocomplete="off" placeholder="" />
-                      <label for="destino" class="text-gray-700">Clave SAT</label>
+                          class="form-control" id="cp_pedimento" autocomplete="off" placeholder="" />
+                      <label for="cp_pedimento" class="text-gray-700">Pedimento</label>
                   </div>
               </div>
             </div>
@@ -116,8 +116,8 @@
                   </span>
                   <div class="form-floating">
                       <input @if ($action == 'editar') value="" @endif type="text"
-                          class="form-control" id="destino" autocomplete="off" placeholder="" />
-                      <label for="destino" class="text-gray-700">Pedimento</label>
+                          class="form-control" id="cp_clase_pedimento" autocomplete="off" placeholder="" />
+                      <label for="cp_clase_pedimento" class="text-gray-700">Clase Pedimento</label>
                   </div>
               </div>
             </div>
@@ -134,8 +134,8 @@
                   </span>
                   <div class="form-floating">
                       <input @if ($action == 'editar') value="" @endif type="text"
-                          class="form-control" id="destino" autocomplete="off" placeholder="" />
-                      <label for="destino" class="text-gray-700">Clase Ped</label>
+                          class="form-control" id="cp_clave_sat" autocomplete="off" placeholder="" />
+                      <label for="cp_clave_sat" class="text-gray-700">Clave SAT</label>
                   </div>
               </div>
             </div>
@@ -152,8 +152,8 @@
                   </span>
                   <div class="form-floating">
                       <input @if ($action == 'editar') value="" @endif type="text"
-                          class="form-control" id="destino" autocomplete="off" placeholder="" />
-                      <label for="destino" class="text-gray-700">Cantidad</label>
+                          class="form-control" id="cp_cantidad" autocomplete="off" placeholder="" />
+                      <label for="cp_cantidad" class="text-gray-700">Cantidad</label>
                   </div>
               </div>
             </div>
@@ -170,8 +170,98 @@
                   </span>
                   <div class="form-floating">
                       <input @if ($action == 'editar') value="" @endif type="text"
-                          class="form-control" id="destino" autocomplete="off" placeholder="" />
-                      <label for="destino" class="text-gray-700">Valor</label>
+                          class="form-control" id="cp_valor" autocomplete="off" oninput="allowOnlyDecimals(event)" placeholder="" />
+                      <label for="cp_valor" class="text-gray-700">Valor</label>
+                  </div>
+              </div>
+            </div>
+
+            <div class="col-3">
+              <div class="input-group  mb-5">
+                  <span class="input-group-text" id="basic-addon1">
+                      <i class="ki-duotone ki-route fs-1">
+                          <span class="path1"></span>
+                          <span class="path2"></span>
+                          <span class="path3"></span>
+                          <span class="path4"></span>
+                      </i>
+                  </span>
+                  <div class="form-floating">
+                      <input @if ($action == 'editar') value="" @endif type="text"
+                          class="form-control" id="cp_moneda_valor" autocomplete="off" placeholder="" />
+                      <label for="cp_moneda_valor" class="text-gray-700">Moneda</label>
+                  </div>
+              </div>
+            </div>
+
+            <div class="col-3">
+              <div class="input-group  mb-5">
+                  <span class="input-group-text" id="basic-addon1">
+                      <i class="ki-duotone ki-route fs-1">
+                          <span class="path1"></span>
+                          <span class="path2"></span>
+                          <span class="path3"></span>
+                          <span class="path4"></span>
+                      </i>
+                  </span>
+                  <div class="form-floating">
+                      <input @if ($action == 'editar') value="" @endif type="text"
+                          class="form-control" id="cp_contacto_entrega" autocomplete="off" placeholder="" />
+                      <label for="cp_contacto_entrega" class="text-gray-700">Tel. Contacto entrega</label>
+                  </div>
+              </div>
+            </div>
+
+            <div class="col-3">
+              <div class="input-group  mb-5">
+                  <span class="input-group-text" id="basic-addon1">
+                      <i class="ki-duotone ki-route fs-1">
+                          <span class="path1"></span>
+                          <span class="path2"></span>
+                          <span class="path3"></span>
+                          <span class="path4"></span>
+                      </i>
+                  </span>
+                  <div class="form-floating">
+                      <input @if ($action == 'editar') value="" @endif type="text"
+                          class="form-control fechas" id="cp_fecha_tentativa_entrega" autocomplete="off" placeholder="" />
+                      <label for="cp_fecha_tentativa_entrega" class="text-gray-700">Fecha tentativa entrega</label>
+                  </div>
+              </div>
+            </div>
+
+            <div class="col-3">
+              <div class="input-group  mb-5">
+                  <span class="input-group-text" id="basic-addon1">
+                      <i class="ki-duotone ki-route fs-1">
+                          <span class="path1"></span>
+                          <span class="path2"></span>
+                          <span class="path3"></span>
+                          <span class="path4"></span>
+                      </i>
+                  </span>
+                  <div class="form-floating">
+                      <input @if ($action == 'editar') value="" @endif  type="time"
+                          class="form-control" id="cp_hora_tentativa_entrega" autocomplete="off" placeholder="" />
+                      <label for="cp_hora_tentativa_entrega" class="text-gray-700">Hora tentativa entrega</label>
+                  </div>
+              </div>
+            </div>
+
+            <div class="col-12">
+              <div class="input-group  mb-5">
+                  <span class="input-group-text" id="basic-addon1">
+                      <i class="ki-duotone ki-route fs-1">
+                          <span class="path1"></span>
+                          <span class="path2"></span>
+                          <span class="path3"></span>
+                          <span class="path4"></span>
+                      </i>
+                  </span>
+                  <div class="form-floating">
+                      <input @if ($action == 'editar') value="" @endif type="text"
+                          class="form-control" id="cp_comentarios" autocomplete="off" placeholder="" />
+                      <label for="cp_comentarios" class="text-gray-700">Comentarios Carta Porte</label>
                   </div>
               </div>
             </div>
