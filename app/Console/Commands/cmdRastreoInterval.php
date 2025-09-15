4ef6330ec9e79d74b1b9bbbed6008f75864d8e7e
carlos-alejandro89 <37030298+carlos-alejandro89@users.noreply.github.com>
2025-09-15 17:36:07 -0600

    Update cmdRastreoInterval.php

--- app/Console/Commands/cmdRastreoInterval.php
+++ app/Console/Commands/cmdRastreoInterval.php
@@ -1,29 +1,2 @@
 <?php
 
-namespace App\Console\Commands;
-
-use Illuminate\Console\Command;
-//use App\Models\RastreoIntervals; 
- //use App\Services\UbiService;
-//use App\Models\coordenadashistorial;
-
-class RevisaRegistrosRastreo extends Command
-{
-    protected $signature = 'rastreo:intervalConfig';
-
-    protected $description = 'Revisa la base de datos las cotizaciones planeadas y si encuentra ejecuta el rastro';
-
-   
-
-        public function __construct()
-        {
-    
-           
-        }
-
-    public function handle()
-    {
-            
-
-    }
-}
