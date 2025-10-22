f263515ab7ddc4e3b7c90e7024e34040db1dc137
carlosalejandro <carlos.alejandro89@outlook.com>
2025-10-22 00:35:39 -0600

    Propuesta nueva interfaz

--- resources/views/bancos/index.blade.php
+++ resources/views/bancos/index.blade.php
@@ -1,120 +1,162 @@
-@extends('layouts.app')
+@extends('layout.main')
 
-@section('template_title')
-    Bancos
+@section('title-window')
+
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center;">
-
-                            <h2 id="card_title">
-                                Bancos
-                            </h2>
-
-                            <div class="float-right">
-                                @can('bancos-create')
-                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                        data-bs-target="#bancoModal"
-                                        style="background: {{ $configuracion->color_boton_add }}; color: #ffff">
-                                        <i class="fa fa-fw fa-plus"></i> Crear
-                                    </button>
-                                @endcan
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="card-body">
-                        <div class="row">
-                            @foreach ($bancos as $item)
-                                <div class="col-lg-4 mt-lg-0 mt-4">
-                                    @can('bancos-entrar')
-                                        <a href="{{ route('edit.bancos', $item->id) }}">
-                                        @endcan
-                                        <div class="card bg-transparent shadow-xl">
-                                            <div class="overflow-hidden position-relative border-radius-xl"
-                                                style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/card-visa.jpg');">
-
-                                                {{-- 🔥 Aquí está el badge, lo dejamos igual --}}
-                                                @if ($item->cuenta_global)
-                                                    <span class="badge bg-success position-absolute"
-                                                        style="top: 10px; left: 10px; z-index: 10;">
-                                                        Cuenta Global
-                                                    </span>
-                                                @endif
-                                                @if ($item->banco_1)
-                                                    <span class="badge bg-danger  position-absolute"
-                                                        style="top: 10px; left: 10px; z-index: 10;">
-                                                        Banco 1
-                                                    </span>
-                                                @endif
-
-                                                <span class="mask bg-gradient-dark"></span>
-
-                                                <div class="card-body position-relative z-index-1 p-3">
-
-                                                    {{-- 🔥 Agregar un pequeño espacio si es Cuenta Global --}}
-                                                    @if ($item->cuenta_global)
-                                                        <div style="height: 20px;"></div> {{-- pequeño salto --}}
-                                                    @endif
-                                                    @if ($item->banco_1)
-                                                        <div style="height: 20px;"></div> {{-- pequeño salto --}}
-                                                    @endif
-
-                                                    <h4 class="text-white mb-0">{{ $item->nombre_beneficiario }}</h4>
-                                                    <h5 class="text-white mt-4 mb-5 pb-2">
-                                                        {{ chunk_split($item->clabe, 4, ' ') }}
-                                                    </h5>
-                                                    <div class="d-flex">
-                                                        <div class="d-flex">
-                                                            <div class="me-4">
-                                                                <p class="text-white text-sm opacity-8 mb-0">Nombre Banco
-                                                                </p>
-                                                                <h6 class="text-white mb-0">{{ $item->nombre_banco }}</h6>
-                                                            </div>
-                                                            <div>
-                                                                <p class="text-white text-sm opacity-8 mb-0">Dinero en banco
-                                                                </p>
-                                                                <h6 class="text-white mb-0">
-                                                                    ${{ number_format($item->saldo, 0, '.', ',') }}
-                                                                </h6>
-                                                            </div>
-                                                        </div>
-                                                        <div
-                                                            class="ms-auto w-20 d-flex align-items-end justify-content-end">
-                                                            <img class="w-60 mt-2"
-                                                                src="../../assets/img/logos/mastercard.png" alt="logo">
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-                                            </div>
-                                        </div>
-                                        @can('bancos-entrar')
-                                        </a>
-                                    @endcan
-                                </div>
-                            @endforeach
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
+<div class="kt-card rounded-xl mb-4">
+        <div class="flex items-center flex-wrap py-3 sm:flex-wrap justify-between grow gap-2 p-5 rtl:[background-position:-30%_41%] [background-position:121%_41%] bg-no-repeat bg-[length:660px_310px] upgrade-bg">
+         <div class="flex items-center gap-4">
+          <div class="relative size-[50px] shrink-0">
+  
+           <div class="absolute leading-none ">
+            <img src="{{asset('/asset/media/bancos/azteca.jpg')}}" alt="">
+           </div>
+          </div>
+          <div class="flex flex-col gap-1.5">
+           <div class="flex items-center flex-wrap gap-2.5">
+            <a class="text-base font-medium text-mono hover:text-primary" href="#">
+             Catalina Castañeda
+            </a>
+            <span class="kt-badge kt-badge-sm kt-badge-outline">
+             Banco Azteca
+            </span>
+           </div>
+           <div class="text-sm text-foreground">
+           Núm Cuenta: 1238469344
+            <br>
+            Clabe: 072180012384693448
+           </div>
+          </div>
+         </div>
+         <div class="flex items-center gap-1.5">
+          <button class="kt-btn kt-btn-mono">
+           Configuración
+          </button>
+          <a class="kt-btn kt-btn-ghost" href="#">
+           Movimiento bancario
+          </a>
+         </div>
         </div>
-    </div>
-    @include('bancos.modal_create')
+       </div>
+
+
+        <div class="kt-card mb-4">
+         <div class="kt-card-content">
+          <div class="flex lg:px-10 py-1.5 gap-2">
+           <div class="grid grid-cols-1 place-content-center flex-1 gap-1 text-center">
+            <span class="text-mono text-2xl lg:text-2xl leading-none font-semibold">
+            $927,986.06
+            </span>
+            <span class="">
+                 <span class="kt-badge rounded-full kt-badge-outline kt-badge-primary gap-1 items-center">
+                    <span class="kt-badge-dot size-2.0">
+                    </span>
+                    Saldo Inicial
+                 </span>
+            </span>
+           </div>
+           <span class="not-last:border-e border-e-input my-1">
+           </span>
+           <div class="grid grid-cols-1 place-content-center flex-1 gap-1 text-center">
+            <span class="text-mono text-2xl lg:text-2xl leading-none font-semibold">
+             $0.00
+            </span>
+            <span class="">
+                 <span class="kt-badge rounded-full kt-badge-outline kt-badge-danger gap-1 items-center">
+                    <span class="kt-badge-dot size-2.0">
+                    </span>
+                    Ingresos
+                 </span>
+            </span>
+           </div>
+           <span class="not-last:border-e border-e-input my-1">
+           </span>
+           <div class="grid grid-cols-1 place-content-center flex-1 gap-1 text-center">
+            <span class="text-mono text-2xl lg:text-2xl sm:text-sm leading-none font-semibold">
+             $1,000.00
+            </span>
+            <span class="">
+                 <span class="kt-badge rounded-full kt-badge-outline kt-badge-warning gap-1 items-center">
+                    <span class="kt-badge-dot size-2.0">
+                    </span>
+                    Egresos
+                 </span>
+            </span>
+           </div>
+           <span class="not-last:border-e border-e-input my-1">
+           </span>
+           <div class="grid grid-cols-1 place-content-center flex-1 gap-1 text-center">
+            <span class="text-mono text-2xl lg:text-2xl leading-none font-semibold">
+            $927,985.06
+            </span>
+            <span class="">
+                 <span class="kt-badge rounded-full kt-badge-outline kt-badge-success gap-1 items-center">
+                    <span class="kt-badge-dot size-2.0">
+                    </span>
+                    Saldo Final
+                 </span>
+            </span>
+           </div>
+           <span class="not-last:border-e border-e-input my-1">
+           </span>
+          </div>
+         </div>
+        </div>
+
+        <div class="mb-4" id="example"></div>
+
+
 @endsection
 
-@section('datatable')
-    <script>
-        const dataTableSearch = new simpleDatatables.DataTable("#datatable-search", {
-            searchable: true,
-            fixedHeight: false
-        });
-    </script>
-@endsection
+@push('custom')
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/handsontable/styles/handsontable.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/handsontable/styles/ht-theme-main.min.css" />
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/handsontable/dist/handsontable.full.min.js"></script>
+<script>
+    document.addEventListener('DOMContentLoaded',()=>{
+        const alto = window.innerHeight;
+        const container = document.querySelector('#example');
+
+const hot = new Handsontable(container, {
+  // theme name with obligatory ht-theme-* prefix
+  themeName: 'ht-theme-main-dark-auto',
+  colHeaders: ['FECHA','DESCRIPCIÓN','REFERENCIA',  'INGRESOS', 'EGRESOS'],
+  data: [
+    
+    ['25 AGO 2025', 'LIQUIDACIÓN OPERADOR - RIGOBERTO', 'NOMINA 0200', 0, 250],
+    ['30 SEPT 2025', 'DINERO PARA VIAJE - ALIMENTOS', '#MGBU3122560', 0, 250],
+    ['10 OCT 2025', 'GASTO DE VIAJE - DIESEL', '#MEDU4532895', 0, 500]
+  ],
+  rowHeaders: true,
+  columns:[{readOnly:false},{readOnly:false },{readOnly:false},
+    {
+      readOnly:false,
+      type: 'numeric',
+      numericFormat: {
+        pattern: '$ 0,0.00',
+        culture: 'en-US'
+      }
+    },
+    {
+      readOnly:false,
+      type: 'numeric',
+      numericFormat: {
+        pattern: '$ 0,0.00',
+        culture: 'en-US'
+      }
+    }
+],
+  height: 'auto',
+  stretchH: 'all',
+  autoWrapRow: true,
+  autoWrapCol: true,
+  licenseKey: 'non-commercial-and-evaluation' // for non-commercial use only
+});
+    })
+   
+
+</script>
+@endpush
---
+++ resources/views/layout/title-window.blade.php
@@ -0,0 +1,15 @@
+<div class="flex flex-wrap items-center lg:items-end justify-between gap-5 pb-1">
+    <div class="flex flex-col justify-center gap-2">
+        <h1 class="text-xl font-medium leading-none text-mono">
+            Modulo bancario
+        </h1>
+        <div class="flex items-center gap-2 text-sm font-normal text-secondary-foreground">
+            Movimientos en cuenta de banco
+        </div>
+    </div>
+    <div class="flex items-center gap-2.5">
+            <a class="kt-btn kt-btn-outline" href="#">
+                View Profile
+            </a>
+    </div>
+</div>
