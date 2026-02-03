b2ccc94bdbe782bdb5ebf4dc2aa43b191870c235
Aymer Ross <45677266+DevRousez@users.noreply.github.com>
2026-02-03 03:26:23 -0600

    Add route for storing Edocuenta

--- routes/web.php
+++ routes/web.php
@@ -435,6 +435,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::post('cuentas/cobrar/confirmar_pagos', [App\Http\Controllers\CuentasCobrarController::class, 'aplicar_pagos'])->name('confirmar.cobrar');
     Route::patch('cuentas/cobrar/update/{id}', [App\Http\Controllers\CuentasCobrarController::class, 'update'])->name('update.cobrar');
     Route::post('cuentas/cobrar/update/varios', [App\Http\Controllers\CuentasCobrarController::class, 'update_varios'])->name('update_varios.cobrar');
+    Route::post('/reporteria/cxp/EdoCuenta/store', [App\Http\Controllers\CuentasCobrarController::class, 'storeEdocuenta'])->name('storeEdocuenta');
 
     // ==================== C U E N T A S  P O R  P A G A R ====================
     Route::get('cuentas/pagar', [App\Http\Controllers\CuentasPagarController::class, 'index'])->name('index.pagar');
